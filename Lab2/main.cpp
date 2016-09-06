9d223b2bc94304694c0abc97f0d1f19fefa59bc0
Dennis Wildmark <d.wildmark@gmail.com>
2016-09-06 15:10:40 +0200

    Task 2 done

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -2,10 +2,38 @@
 #include <cmath>
 
 void task1(int n);
+void task2();
+void task3();
+void Substitute2(std::string& iostring,
+                 const std::string& before,
+                 const std::string& after);
+char* Substitute3(char* const input,
+                  const char before,
+                  char* const after);
+
 
 int main() {
-    std::cout << "Starting task 1." << std::endl;
-    task1(100);
+    int a;
+    std::cout << "Which task do you want to run?" << std::endl;
+    std::cin >> a;
+    switch(a) {
+        case 1:
+            std::cout << "Starting task 1." << std::endl;
+            task1(100);
+            break;
+        case 2:
+            std::cout << "Starting task 2." << std::endl;
+            task2();
+            break;
+        case 3:
+            std::cout << "Starting task 3." << std::endl;
+            task3();
+            break;
+        case 4:
+            std::cout << "Starting task 4." << std::endl;
+            break;
+    }
+
     return 0;
 }
 
@@ -44,4 +72,26 @@ void task1(int n) {
         }
     }
     std::cout << std::endl;
+}
+
+void task2() {
+    std::string input = "Hejjj";
+    Substitute2(input, "ej", "eje");
+    std::cout << input << std::endl;
+}
+
+void task3() {
+
+}
+
+void Substitute2(std::string& iostring,
+                 const std::string& before,
+                 const std::string& after) {
+    int index = (int)iostring.find(before);
+    std::string res = iostring.substr(0, index) + after + iostring.substr(index + before.length());
+    iostring = res;
+}
+
+char* Substitute3(char* const input, const char before, char* const after) {
+
 }
